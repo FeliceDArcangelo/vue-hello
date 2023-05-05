1dc788bcbf014254c85d86b95bcbdf89ebbe6bb6
FeliceDArcangelo <felicedarcangelo25@gmail.com>
2023-05-05 15:33:53 +0200

    ...

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const app = Vue.createApp({
     data (){
         return {
             title: 'Hey I\'m using vuejs ;)',
+            titleCss: 'text-center p-1',
             titleImg: 'n_three.jpeg',
+            titleImgCss: 'w-50'
+        }
+    },
+    methods: {
+        changeImg(){
+            if (this.titleImg == 'n_three.jpeg'){
+                this.titleImg = 'n_two.jpeg'
+            } else {
+                this.titleImg = 'n_three.jpeg'
+            }
         }
     }
 })
