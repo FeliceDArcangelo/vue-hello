c0bc0e4f1a61772eacd28cd377b93cacdfdf3b82
FeliceDArcangelo <felicedarcangelo25@gmail.com>
2023-05-05 15:16:33 +0200

    w vuejs

---
+++ script.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data (){
+        return {
+            title: 'Hey I\'m using vuejs ;)',
+            titleImg: 'n_three.jpeg',
+        }
+    }
+})
+
+app.mount('#root')
